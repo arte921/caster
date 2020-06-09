af579f5fd92ba2dfb074f4e001f00ad8e60fe29d
Ard-Jan van Etten <ardjanve@gmail.com>
2020-06-09 16:39:07 +0200

    added dns

--- client/main.js
+++ client/main.js
@@ -75,7 +75,7 @@ function lockChangeAlert() {
 
 window.requestAnimationFrame(drawframe)
 
-var socket = new WebSocket("wss://213.247.103.127:30000")
+var socket = new WebSocket("ws://http://arte921.duckdns.org:30000/")
 socket.addEventListener("open", function(event){
   socket.send(JSON.stringify(new Wall(player.shield.pos1, player.shield.pos2, player.color)))
   console.log("connected")
