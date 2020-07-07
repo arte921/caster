118215966afe02a78d926264b11056c967b82c62
Ard-Jan van Etten <ardjanve@gmail.com>
2020-07-07 12:15:55 +0200

    docs

---
+++ docs/main.js
@@ -0,0 +1,74 @@
+var mcbwidth = window.innerWidth
+var mcbheight = window.innerHeight
+var canvas = document.getElementById("canvas")
+var ctx = canvas.getContext("2d")
+canvas.width = mcbwidth
+canvas.height = mcbheight
+var bgcolor = "#000000"
+
+const heightfactor = 0.1
+
+let fov = 70
+
+var walls = []
+walls.push(new Wall(new Point(10,10),new Point(10,90),"rgb(" + Math.random() * 255 + "," + Math.random() * 255 + "," + Math.random() * 255 + ")"))
+walls.push(new Wall(new Point(10,10),new Point(90,10),"rgb(" + Math.random() * 255 + "," + Math.random() * 255 + "," + Math.random() * 255 + ")"))
+walls.push(new Wall(new Point(90,90),new Point(10,90),"rgb(" + Math.random() * 255 + "," + Math.random() * 255 + "," + Math.random() * 255 + ")"))
+walls.push(new Wall(new Point(90,90),new Point(90,10),"rgb(" + Math.random() * 255 + "," + Math.random() * 255 + "," + Math.random() * 255 + ")"))
+
+for(let i = 0;i<10;i++){
+	walls.push(new Wall(new Point(Math.random()*100,Math.random()*100),new Point(Math.random()*100,Math.random()*100),"rgb(" + Math.random() * 255 + "," + Math.random() * 255 + "," + Math.random() * 255 + ")"))
+}
+var otherplayers = []
+
+let player = new Player(new Point(40,40),Math.PI * 1.3,fov/180*Math.PI)
+
+let t0 = performance.now()
+function drawframe(){
+  let dt = performance.now() - t0
+  t0 = performance.now()
+  player.render()
+  document.getElementById("title").innerHTML = Math.round(1/(dt/1000)) + " fps"
+  window.requestAnimationFrame(drawframe)
+}
+
+function mousemoved(e){
+  player.bearing += Math.PI*e.movementX/mcbwidth
+}
+
+document.onkeypress = function(e){
+  switch(e.key){
+    case "w":
+      player.move(new Point(player.pos.x + Math.cos(player.bearing) * player.speed, player.pos.y + Math.sin(player.bearing) * player.speed))
+    break;
+    case "s":
+      player.move(new Point(player.pos.x - Math.cos(player.bearing) * player.speed, player.pos.y - Math.sin(player.bearing) * player.speed))
+
+    break;
+    case "a":
+      player.move(new Point(player.pos.x - Math.cos(player.bearing + Math.PI / 2) * player.speed, player.pos.y - Math.sin(player.bearing + Math.PI / 2) * player.speed))
+
+    break;
+    case "d":
+      player.move(new Point(player.pos.x + Math.cos(player.bearing + Math.PI / 2) * player.speed, player.pos.y + Math.sin(player.bearing + Math.PI / 2) * player.speed))
+    break;
+
+  }
+}
+
+canvas.requestPointerLock = canvas.requestPointerLock || canvas.mozRequestPointerLock
+document.exitPointerLock = document.exitPointerLock || document.mozExitPointerLock
+canvas.onclick = () => {
+  canvas.requestPointerLock()
+}
+document.addEventListener('pointerlockchange', lockChangeAlert, false)
+document.addEventListener('mozpointerlockchange', lockChangeAlert, false)
+function lockChangeAlert() {
+  if (document.pointerLockElement === canvas || document.mozPointerLockElement === canvas) {
+    document.addEventListener("mousemove", mousemoved, false)
+  } else {
+    document.removeEventListener("mousemove", mousemoved, false)
+  }
+}
+
+window.requestAnimationFrame(drawframe)
---
+++ docs/player.js
@@ -0,0 +1,63 @@
+class Player{
+  constructor(pos, bearing, fov){ //bearing and fov in radians
+    this.pos = pos
+    this.bearing = bearing
+    this.fov = fov
+    this.speed = 1
+    this.shieldwidth = 2/(Math.tan(0.5*(Math.PI-fov)))
+    this.minimap = new Minimap(0, 0, 80, 100, 100, walls) //minx, miny, xsize, scenewidth, sceneheight, walls
+    this.shield = new Shield(this)
+    this.color = "rgb(" + Math.random() * 255 + "," + Math.random() * 255 + "," + Math.random() * 255 + ")"
+    //this.noprod = {}
+  }
+
+  move(dest){
+    let canmove = true
+    walls.forEach(wall => {
+      let denominator = (this.pos.x-dest.x)*(wall.y1-wall.y2)-(this.pos.y-dest.y)*(wall.x1-wall.x2)
+      let t = ((this.pos.x-wall.x1)*(wall.y1-wall.y2)-(this.pos.y-wall.y1)*(wall.x1-wall.x2))/denominator
+      let u = -((this.pos.x-dest.x)*(this.pos.y-wall.y1)-(this.pos.y-dest.y)*(this.pos.x-wall.x1))/denominator
+      if(t >= 0 && t <= 1 && u >= 0 && u <= 1) canmove = false
+    })
+    if(canmove) this.pos = dest
+  }
+
+  render(){
+    ctx.clearRect(0,0,mcbwidth,mcbheight)
+    this.shield = new Shield(this)
+    this.minimap.render(this.pos, this.shield)
+    //console.clear()
+    for(let i=0; i<mcbwidth; i++){
+      let ray = new Ray(this.pos,this.shield.rayintersect(i,mcbwidth))
+      //console.log(ray)
+      let record, recordintersection
+      let recorddistance = Infinity
+
+      let scene = walls.concat(otherplayers)
+
+      scene.forEach(wall => {
+        //this.noprod++
+
+        let intersection = ray.intersect(wall)
+        if(intersection != null){
+          let distance = intersection.distanceto(this.pos)
+          // && wall.color != player.color
+          if(distance < recorddistance){
+            record = wall
+            recorddistance = distance
+            recordintersection = intersection
+          }
+        }
+      })
+
+      if(recorddistance != Infinity){
+        //let wallheight = (mcbheight - Math.pow(recorddistance,-1)) * heightfactor
+        let wallheight = Math.pow(recorddistance,-1) * 1000
+        this.minimap.drawline(this.pos,recordintersection,record.color)
+        //console.log(record)
+        ctx.fillStyle = record.color
+        ctx.fillRect(i,mcbheight/2 - wallheight/2, 1, wallheight)
+      }
+    }
+  }
+}
